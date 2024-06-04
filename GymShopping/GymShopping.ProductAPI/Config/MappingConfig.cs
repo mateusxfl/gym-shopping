d47fa9c04797648d9e7d4f14dd072de786c1bb21
Mateus Fonseca Lima <mateus.fonseca@fitbank.com.br>
2024-06-04 13:11:48 -0300

    feat: configurando auto mapper.

---
+++ GymShopping/GymShopping.ProductAPI/Config/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using GymShopping.ProductAPI.Data.ValueObjects;
+using GymShopping.ProductAPI.Model;
+
+namespace GymShopping.ProductAPI.Config
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductVO, Product>();
+                config.CreateMap<Product, ProductVO>();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
