e7a9681367327b0d8e5b703d848fd8cdae1ad4ff
Mateus Fonseca Lima <mateus.fonseca@fitbank.com.br>
2024-06-04 12:54:09 -0300

    feat: criando interface para o repositório de produtos.

---
+++ GymShopping/GymShopping.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using GymShopping.ProductAPI.Data.ValueObjects;
+
+namespace GymShopping.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductVO>> FindAll();
+        Task<ProductVO> FindBydId(long id);
+        Task<ProductVO> Create(ProductVO product);
+        Task<ProductVO> Update(ProductVO product);
+        Task<bool> Delete(long id);
+    }
+}
