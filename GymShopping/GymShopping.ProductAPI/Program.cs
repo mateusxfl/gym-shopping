b8b564182ddce796decd21b837a65d5895fbc87e
Mateus Fonseca Lima <mateus.fonseca@fitbank.com.br>
2024-06-04 11:23:29 -0300

    feat: criando conexão com o banco de dados.

--- GymShopping/GymShopping.ProductAPI/Program.cs
+++ GymShopping/GymShopping.ProductAPI/Program.cs
@@ -1,6 +1,16 @@
+using GymShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connection = builder.Configuration["MySqlConnection:MySqlConnectionString"];
+
+builder.Services.AddDbContext<MySQLContext>(options => options
+    .UseMySql(
+        connection,
+        new MySqlServerVersion(new Version(8, 0, 29))));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
